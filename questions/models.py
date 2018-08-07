359900e6bd773af322cc7d3194eda43b291d3b71
Katie Maxwell <katie@codeinstitute.net>
2018-08-07 13:22:02 +0000

    added a question model

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=250, blank=True, null=True)
+    correct_answer = models.CharField(max_length=250, blank=True, null=True)
+    wrong_answer1 = models.CharField(max_length=250, blank=True, null=True)
+    wrong_answer2 = models.CharField(max_length=250, blank=True, null=True)
+    wrong_answer3 = models.CharField(max_length=250, blank=True, null=True)
+    
+    def __str__(self):
+        return self.question
